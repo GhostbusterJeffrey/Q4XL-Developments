e5305b55bdbbdc3f809bb92a4eb5583a3176ecb2
SuperDino484 <39782317+SuperDino484@users.noreply.github.com>
2021-03-14 14:00:52 -0600

    Updated canvas js
    Fixed the angle indicator being offset and added speed. Added a setting in code called "anglebarheight" which is the height in pixels of each bar. This can be changed and the pitch indicator will change dynamically to the new height of the indicators. Added 3 variables called "planePitch", "planeRoll", "planeSpeed" which is the variables that you can change to change the indicators of the plane.

--- Q400/PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/Q400/PFD/canvas.js
+++ Q400/PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/Q400/PFD/canvas.js
@@ -3,31 +3,37 @@ var canvas = document.querySelector('canvas');
 canvas.width = 560;
 canvas.height = 420;
 
-var curX = 0;
-var curY = 0;
+
+var planePitch = 10;
+var planeRoll = 0;
+var planeSpeed = 0;
 
 var w = canvas.width;
 var h = canvas.height;
+var anglebarheight = 25;
 
 var c = canvas.getContext('2d');
 
-// Top Bar
-c.beginPath();
-c.rect(136, 0, 3, 40);
-c.rect(378, 0, 3, 40);
-c.fillStyle = 'white';
-c.fill();
-c.closePath();
-
-drawSpeedMeter();
-drawAngleIndicator(235, 218, 125, 20);
+drawTopBar();
+drawSpeedMeter(12, 84);
 drawAltitudeMeter();
+drawAngleIndicator(235, 218, 125, 20);
 
 console.log(canvas);
+
+function drawTopBar() {
+    c.beginPath();
+    c.rect(136, 0, 3, 40);
+    c.rect(378, 0, 3, 40);
+    c.fillStyle = 'white';
+    c.fill();
+    c.closePath();
+}
 
 function drawAltitudeMeter() {
     c.translate(405, 84);
     c.save();
+    
     c.beginPath();
     var width = 68;
     var height = 277;
@@ -36,8 +42,6 @@ function drawAltitudeMeter() {
     c.fill();
     c.closePath();
     c.clip();
-
-    // Speed here
 
     // Speed black bar triangle
     var x1 = 50;
@@ -64,6 +68,7 @@ function drawAltitudeMeter() {
     c.textAlign = 'left';
     // Text align right with 0.5 * width with font size 16
     c.fillText(250, 0.5 * width, (height) / 2, 100);
+
     c.restore();
 
     // Two white bars
@@ -77,13 +82,14 @@ function drawAltitudeMeter() {
     c.fillStyle = 'rgba(255, 255, 255, 0.6)';
     c.fill();
     c.closePath();
+
     c.translate(-405, -84);
 }
 
-function drawAngleIndicator(x, y, radius, cutoff) {
+function drawAngleIndicator(xpos, ypos, radius, cutoff) {
     c.save();
     c.beginPath();
-    c.translate(x, y);
+    c.translate(xpos, ypos);
     // Set clipping for the middle attitude indicator
     var circlePath = new Path2D();
     circlePath.arc(0, 0, radius, 0, 2 * Math.PI, true);
@@ -95,14 +101,14 @@ function drawAngleIndicator(x, y, radius, cutoff) {
 
     // Draws the angle and rotation screen
     // Angle is in degrees
-    drawAttitude(x, 0, 0, radius);
+    drawAttitude(xpos, planePitch, planeRoll, radius);
 
-    c.translate(-x, -y);
+    c.translate(-xpos, -ypos);
     c.restore();
 }
 
-function drawAttitude(x, pitch, roll, radius) {
-    var y = pitch * 4.5;
+function drawAttitude(xpos, pitch, roll, radius) {
+    var y = pitch * ((anglebarheight / 10) * 2);
     c.rotate(toRadians(roll));
 
     // Top blue area
@@ -127,15 +133,15 @@ function drawAttitude(x, pitch, roll, radius) {
     c.closePath();
 
     // Angle indicators
+    var barheight = 3;
     for(var i = -10; i < 10; i++) {
         if(i == 0) continue;
-        var yheight = (i * 23);
-        var wwidth = (i % 2 == 0) ? 60 : 30;
         c.beginPath();
-        c.rect(-wwidth / 2, yheight + y, wwidth, 3);
-        c.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        var yheight = (i * anglebarheight);
+        var wwidth = (i % 2 == 0) ? 60 : 30;
+        c.rect(-wwidth / 2, y + yheight - (barheight / 2), wwidth, barheight);
+        c.fillStyle = 'rgba(255, 255, 255, 1)';
         c.fill();
-        c.closePath();
         if(i % 2 == 0) {
             c.fillStyle = 'white';
             c.font = '16px Arial';
@@ -145,27 +151,29 @@ function drawAttitude(x, pitch, roll, radius) {
             angle *= (i > 0) ? -1 : 1;
             c.fillText(angle, -wwidth, yheight + 8 + y);
         }
+        c.closePath();
     }
 
+    c.rotate(-toRadians(roll));
     // Plane position relative to the indicators
+    var yoffset_blackbars = -2.5;
     c.beginPath();
-    c.rect(-radius + 40, 0, 70, 5);
-    c.rect(radius - 110, 0, 70, 5);
-    c.rect(-2.5, 0, 5, 5);
-    c.rect(-15, 0, 5, 10);
-    c.rect(10, 0, 5, 10);
+    c.rect(-radius + 40, yoffset_blackbars, 70, 5);
+    c.rect(radius - 110, yoffset_blackbars, 70, 5);
+    c.rect(-2.5, yoffset_blackbars, 5, 5);
+    c.rect(-15, yoffset_blackbars, 5, 10);
+    c.rect(10, yoffset_blackbars, 5, 10);
     c.fillStyle = 'rgb(0, 0, 0)';
     c.fill();
     c.closePath();
 
 }
 
-
-function drawSpeedMeter() {
-    c.translate(12, 84);
+function drawSpeedMeter(xpos, ypos) {
+    c.translate(xpos, ypos);
     c.save();
 
-    // Left speed blue bar
+    // Left speed blue bar and clipping
     c.beginPath();
     var width = 68;
     var height = 277;
@@ -176,13 +184,32 @@ function drawSpeedMeter() {
     c.clip();
 
     // Speed here
+    c.beginPath();
+    var speedBarWidth = (1/6) * width;
+    var speedBarHeight = 3;
+    var yadd = planeSpeed * 3;
+    for(var i = 0; i < 50; i++) {
+        var yheight = -i * 30;
+        c.rect(width - speedBarWidth, yheight + (height / 2 - 5) - (speedBarHeight / 2) + yadd, speedBarWidth, speedBarHeight);
+        c.fillStyle = 'rgb(255, 255, 255)';
+        c.fill();
+        if(i % 2 == 0) {
+            c.fillStyle = 'white';
+            c.font = '16px Arial';
+            c.textAlign = 'right';
+            c.fillText(i * 10, 0.5 * width, yheight + (height / 2) + yadd, 100);
+            console.log("HEIGHT: " + (yheight + yadd) + "VAL: " + i * 10);;      
+        }
+    }
+    c.closePath();
 
     // Speed black bar triangle
+    var x1 = 40;
     c.beginPath();
-    c.moveTo(45, height / 2 - 20);
-    c.lineTo(60, height / 2 - 5);
-    c.lineTo(45, height / 2 + 10);
-    c.lineTo(45, height / 2 - 20);
+    c.moveTo(x1, height / 2 - 20);
+    c.lineTo(x1 + 15, height / 2 - 5);
+    c.lineTo(x1, height / 2 + 10);
+    c.lineTo(x1, height / 2 - 20);
     c.fillStyle = 'black';
     c.stroke();
     c.fill();
@@ -190,19 +217,20 @@ function drawSpeedMeter() {
 
     // Speed black bar rectangle
     c.beginPath();
-    c.rect(0, height / 2 - 20, 45, 30);
+    c.rect(0, height / 2 - 20, x1, 30);
     c.fillStyle = 'black';
     c.fill();
     c.stroke();
     c.closePath();
 
+    // Text align right with 0.5 * width with font size 16
     c.fillStyle = 'white';
     c.font = '16px Arial';
     c.textAlign = 'right';
-    // Text align right with 0.5 * width with font size 16
-    c.fillText(0, 0.5 * width, (height) / 2, 100);
-    c.restore();
+    c.fillText(planeSpeed, 0.5 * width, (height) / 2, 100);
 
+    c.restore();
+    
     // Two white rectangles
     c.beginPath();
     c.rect(0, -2, 77, 3);
@@ -214,7 +242,7 @@ function drawSpeedMeter() {
     c.fillStyle = 'rgba(255, 255, 255, 0.6)';
     c.fill();
     c.closePath();
-    c.translate(-12, -84);
+    c.translate(-xpos, -ypos);
 }
 
 function toDegrees(radians) {
